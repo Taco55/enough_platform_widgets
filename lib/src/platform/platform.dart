be99ddbf347d5f96c3a5f82bca1f988704155aa6
Taco Kind <taco.kind@gmail.com>
2024-02-24 14:42:09 +0100

    Remove platform_sliver_appbar from export

--- lib/src/platform/platform.dart
+++ lib/src/platform/platform.dart
@@ -7,7 +7,6 @@ export 'platform_page_scaffold.dart';
 export 'platform_checkbox_list_tile.dart';
 export 'platform_radio_list_tile.dart';
 export 'decorated_platform_textfield.dart';
-export 'platform_sliver_appbar.dart';
 export 'platform_drawer.dart';
 export 'platform_bottom_bar.dart';
 export 'platform_progress_indicator.dart';
